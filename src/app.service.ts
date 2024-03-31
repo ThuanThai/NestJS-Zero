5311dcb794ea31f3ae3653fdd24cf0fa03819947
Jasper Johns <jasper@Jaspers-MacBook-Air.local>
2024-03-31 14:36:34 +0700

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Tai khoan cua ban: 10.000.000$';
   }
 }
