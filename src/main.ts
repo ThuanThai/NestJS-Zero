61693b3154fdbfd450ea0292a0a9a5d9e424582c
Jasper Johns <jasper@Jaspers-MacBook-Air.local>
2024-04-06 00:20:05 +0700

    update CORS

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { NestFactory, Reflector } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
+import { ValidationPipe } from "@nestjs/common";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
-    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.useStaticAssets(join(__dirname, "..", "public"));
     app.useGlobalPipes(new ValidationPipe());
-    app.setBaseViewsDir(join(__dirname, '..', 'views'));
-    app.setViewEngine('ejs');
+    app.setBaseViewsDir(join(__dirname, "..", "views"));
+    app.setViewEngine("ejs");
+    app.enableCors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+    });
     await app.listen(process.env.PORT);
 }
 bootstrap();
